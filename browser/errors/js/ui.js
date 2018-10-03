e71061399ccbcf8907c42583d77fa90008c8d25a
Evgeny <rogue77mev@gmail.com>
2018-10-03 21:43:55 +0500

    init commit

--- browser/errors/js/ui.js
+++ browser/errors/js/ui.js
@@ -1,6 +1,6 @@
 'use strict';
-function handleClick {
-  this.openMail();
+function handleClick() {
+  openMail();
 }
-const button = window.getElementById('open');
+const button = document.getElementById('open');
 button.onclick = handleClick();
